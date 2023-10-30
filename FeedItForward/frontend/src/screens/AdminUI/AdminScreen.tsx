72501dc78656ebe80d2a87653ce2d643c92f515b
Jing Qiang <tohjingqiang@gmail.com>
2023-10-30 17:23:09 +0800

    frontend: add admin management screen

--- FeedItForward/frontend/src/screens/AdminUI/AdminScreen.tsx
+++ FeedItForward/frontend/src/screens/AdminUI/AdminScreen.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { ScreenTitle } from "../../components";
+import { AdminCard } from "../../components/Cards";
 
 export const AdminScreen = () => {
-  return <div>AdminScreen</div>;
+  return (
+    <div>
+      <ScreenTitle title="Admin Management" />
+
+      {/* Cards */}
+      <div className="flex flex-col justify-center items-center mt-12 gap-5">
+        <AdminCard
+          img={{
+            src: "/images/admin-lock.png",
+            alt: "lock"
+          }}
+          title="Verify User"
+          description="Verify a user's application for priority food"
+          href="/admin/verify-user"
+        />
+        <AdminCard
+          img={{
+            src: "/images/admin-ban.png",
+            alt: "hammer"
+          }}
+          title="Ban User"
+          description="Ban a user by their user_id for a specified duration"
+          href="/admin/ban-user"
+        />
+        <AdminCard
+          img={{
+            src: "/images/admin-smiley.png",
+            alt: "smiley face"
+          }}
+          title="Process Reviews"
+          description="Process flagged reviews for deletion"
+          href="/admin/process-reviews"
+        />
+      </div>
+    </div>
+  );
 };
---
+++ FeedItForward/frontend/src/screens/AdminUI/AdminVerifyUserScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScreenTitle } from "../../components";
+
+export const AdminVerifyUserScreen = () => {
+  return (
+    <div>
+      <ScreenTitle title="Verify User" />
+    </div>
+  );
+};
